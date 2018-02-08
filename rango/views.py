9b95fd548d80d98d5cb265c25dd33fad3843972b
2251088M <35376975+2251088M@users.noreply.github.com>
2018-02-08 23:40:02 +0000

    Chapter 5 done

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
+
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    
     return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
-    return render(request, 'rango/about.html')
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
